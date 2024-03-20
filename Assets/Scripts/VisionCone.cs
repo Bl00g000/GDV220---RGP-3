75191c148226d71bcd5cd5a8374c78fea379f68a
Bloo <49353322+Bl00g000@users.noreply.github.com>
2024-03-20 18:55:14 +1300

    Moved raycast start and end Y pos
    rays were being cast over some objects so I moved the start and finish positions down to the bottom of the player model

--- Assets/Scripts/VisionCone.cs
+++ Assets/Scripts/VisionCone.cs
@@ -36,23 +36,28 @@ public class VisionCone : MonoBehaviour
         float fPlayerAngle = Mathf.Atan2(unitTransform.forward.x, unitTransform.forward.z);
         float fInitialAngle = fPlayerAngle - DegToRad(fFOVdeg / 2);
 
-        hitPositions.Clear(); // Clear the list before populating it with new hit positions
+        // Clear the list before populating it with new hit positions
+        hitPositions.Clear();
 
         for (int i = 0; i < iRayCount; i++)
         {
             float fAngleOffset = i * fAngleStep;
             Vector3 rayDirection = new Vector3(Mathf.Sin(fInitialAngle + fAngleOffset), 0, Mathf.Cos(fInitialAngle + fAngleOffset));
 
+            Vector3 playPos = new Vector3(unitTransform.position.x, 0.1f, unitTransform.position.z);
+
             RaycastHit hit;
-            if (Physics.Raycast(unitTransform.position, rayDirection, out hit, fFlashlightRange, hitLayers))
+            if (Physics.Raycast(playPos, rayDirection, out hit, fFlashlightRange, hitLayers))
             {
+                // If there is a hit, add the hit point to the hitpositions
                 hitPositions.Add(hit.point);
-                Debug.DrawRay(unitTransform.position, hit.point - unitTransform.position, Color.magenta);
+                Debug.DrawRay(playPos, hit.point - playPos, Color.blue);
             }
             else
             {
-                hitPositions.Add(unitTransform.position + rayDirection * fFlashlightRange); // If no hit, add point at the end of the range
-                Debug.DrawRay(unitTransform.position, rayDirection * fFlashlightRange, Color.red);
+                // If no hit, add point at the end of the range
+                hitPositions.Add(playPos + rayDirection * fFlashlightRange); 
+                Debug.DrawRay(playPos, rayDirection * fFlashlightRange, Color.red);
             }
         }
     }
