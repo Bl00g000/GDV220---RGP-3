c423b9f68dd3be096be28a0e22bfbf036c505f2a
Bloo <49353322+Bl00g000@users.noreply.github.com>
2024-03-21 14:52:08 +1300

    started implementing flashlight

--- Assets/Scripts/Player/VisionCone.cs
+++ Assets/Scripts/Player/VisionCone.cs
@@ -17,7 +17,6 @@ public class VisionCone : MonoBehaviour
     public float fFlashlightRange = 10.0f;
     public float hitOffset;
     public List<Vector3> hitPositions = new List<Vector3>();
-    public List<Collider> hitColliders = new List<Collider>();
 
     // Start is called before the first frame update
     void Start()
@@ -80,12 +79,6 @@ public class VisionCone : MonoBehaviour
 
                 // adds if no hit back was found or required!
                 hitPositions.Add(hit.point + (hit.point - playPos).normalized * hitOffset);
-
-                // Check if the hit collider already exists in the List so that we only add each hit collider once
-                if (!hitColliders.Contains(hit.collider))
-                {
-                    hitColliders.Add(hit.collider);
-                }
                 continue;
             }
             else
--- Assets/Scripts/Weapons/Flashlight.cs
+++ Assets/Scripts/Weapons/Flashlight.cs
@@ -5,28 +5,47 @@ using UnityEngine;
 public class Flashlight : MonoBehaviour
 {
     VisionCone playerVisionCone;
+    PointsToPlane pointsToPlane;
     public float fFlashLightDPS = 2.0f;
+    Collider[] collisions;
+    public bool bFlashLightActive = false;
 
     // Start is called before the first frame update
     void Start()
     {
         playerVisionCone = gameObject.GetComponent<VisionCone>();
+        pointsToPlane = gameObject.GetComponentInChildren<PointsToPlane>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(1))
+        {
+            bFlashLightActive = true;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            bFlashLightActive = false;
+        }
+        
+        collisions = Physics.OverlapSphere(gameObject.GetComponentInChildren<PlayerMovement>().transform.position, playerVisionCone.fFlashlightRange + 10);
         CheckForEnmiesHit();
     }
 
     void CheckForEnmiesHit()
     {
-        if (playerVisionCone.hitColliders.Count > 0)
+        // check whether the flashlight on otherwise skip function
+        if (!bFlashLightActive) { return; }
+
+        // check through all nearby colliders and check if they are in the light currently
+        foreach (var _collider in collisions) 
         {
-            foreach (Collider _collider in playerVisionCone.hitColliders)
+            if (pointsToPlane.LightContainsObject(_collider.gameObject))
             {
                 if (_collider.CompareTag("Enemy"))
                 {
+                    Debug.Log("enemy hit");
                     EnemyBase enemyBase = _collider.GetComponent<EnemyBase>();
                     enemyBase.bFlashlighted = true;
                     enemyBase.fHealth -= fFlashLightDPS * Time.deltaTime;
