8ff6621914077d1abb898d5b3d9a569e96da0541
Nathan Hunt <nathanpmhunt@yahoo.co.nz>
2024-03-21 16:22:56 +1300

    current scene
    PUSH

--- Assets/Scripts/TechArt/FlashlightGraphics.cs
+++ Assets/Scripts/TechArt/FlashlightGraphics.cs
@@ -1,18 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FlashlightGraphics : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Optional Variables")]
+    public Flashlight attachedFlashlight;
+
+    public List<GameObject> toggleObjects = new List<GameObject>();
+    public List<GameObject> toggleInverseObjects = new List<GameObject>();
+    void Awake()
     {
-        
+        if(attachedFlashlight == null) attachedFlashlight = transform.root.GetComponent<Flashlight>();
+    }
+
+    private void Start()
+    {
+        attachedFlashlight.FlashLightToggle += OnFlashlightToggle;
     }
 
     // Update is called once per frame
-    void Update()
+    void OnFlashlightToggle(bool toggled)
     {
-        
+        foreach (GameObject toggleObj in toggleObjects)
+        {
+            toggleObj.SetActive(toggled);
+        }
+
+        foreach (GameObject toggleObj in toggleInverseObjects)
+        {
+            toggleObj.SetActive(!toggled);
+        }
     }
 }
