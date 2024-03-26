2b5b6c7164e0a2739719e8babf9db7e90312f869
George White <tgeorgew465@gmail.com>
2024-03-26 15:34:25 +1300

    Added helth bar to canvas

---
+++ Assets/Scripts/UI/PlayerHealthBarUI.cs
@@ -0,0 +1,58 @@
+using MPUIKIT;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthBarUI : MonoBehaviour
+{
+    //PlayerData playerData;
+    //EnemyBase enemyBase;
+    public GameObject healthCanvas;
+    public MPImage healthBar;
+    public bool bShowingHealthBar = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if(PlayerData.instance)
+        //{
+            PlayerData.instance.healthBarUI = this;
+        //}
+        HideCanvas();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if(healthBar && PlayerData.instance)
+        {
+
+            if(bShowingHealthBar && PlayerData.instance.fCurrentHealth < PlayerData.instance.fMaxHealth)
+            {
+
+                healthBar.fillAmount = (PlayerData.instance.fCurrentHealth / PlayerData.instance.fMaxHealth);
+
+            }
+        }
+    }
+
+   public void ShowCanvas()
+    {
+        if (healthCanvas)
+        {
+            healthCanvas.SetActive(true);
+        }
+        bShowingHealthBar = true;
+    }
+    public void HideCanvas()
+    {
+       
+        if (healthCanvas)
+        {
+            healthCanvas.SetActive(false);
+        }
+        bShowingHealthBar = false;
+    }
+}
