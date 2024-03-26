b1f5ae0b855415fbb8e77604d788a2762c5b049f
Bloo <49353322+Bl00g000@users.noreply.github.com>
2024-03-26 15:54:40 +1300

    Update Flashlight.cs

--- Assets/Scripts/Weapons/Flashlight.cs
+++ Assets/Scripts/Weapons/Flashlight.cs
@@ -59,7 +59,6 @@ public class Flashlight : MonoBehaviour
     {
         if (Time.timeScale <= 0f) { return; }
         if (!bHasFlashLight) { return; }
-        CheckForEnmiesHit();
 
         // Get the mouse wheel movement speed
         float scrollSpeed = Input.mouseScrollDelta.y;
